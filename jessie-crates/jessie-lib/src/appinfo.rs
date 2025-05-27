558a471076ec4843a7bb18c382986ed1086cb872
EvoPot <evolvedpot@gmail.com>
2025-05-27 11:21:28 +0300

    jessie-lib : derive debug for appinfo structs

--- jessie-crates/jessie-lib/src/appinfo.rs
+++ jessie-crates/jessie-lib/src/appinfo.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 ///AppInfo is a struct that jessie-lib uses to define platform specific behaviour.
 ///You can edit it using config.ron at the root of your project.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct AppInfo {
     linux: LinuxConfig,
     windows: WindowsConfig,
@@ -12,21 +12,21 @@ pub struct AppInfo {
 }
 
 ///A struct for Linux specific configuration.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct LinuxConfig {}
 
 ///A struct for Windows specific configuration.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct WindowsConfig {}
 
 ///A struct for MacOS specific configuration.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct MacOSConfig {}
 
 ///A struct for Android specific configuration.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct AndroidConfig {}
 
 ///A struct for IOS specific configuration.
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct IOSConfig {}
