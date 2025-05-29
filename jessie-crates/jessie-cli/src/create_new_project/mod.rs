b65893f3f49aa05911f7aa74d6103ef77e2af8dd
EvoPot <evolvedpot@gmail.com>
2025-05-29 14:03:14 +0300

    jessie-cli : add jessie-build as a build dependency (lol)

--- jessie-crates/jessie-cli/src/create_new_project/mod.rs
+++ jessie-crates/jessie-cli/src/create_new_project/mod.rs
@@ -17,17 +17,21 @@ pub fn create_new_project(name: &str) {
         .expect("failed to add jessie-lib to the newly created crate");
     Command::new("cargo")
         .arg("add")
+        .arg("--build")
         .arg("jessie-build")
         .current_dir(Path::new(name))
         .status()
         .expect("failed to add jessie-build to the newly created crate");
     create_file_at_dir(name, "config.ron").expect("failed to create config.ron");
     create_file_at_dir(name, "build.rs").expect("failed to create config.ron");
+
+    write(Path::new(name).join("config.ron"), "()")
+        .expect("jessie-build : failed to write to config.ron");
     write(
         Path::new(name).join("build.rs"),
         include_str!("buildtemplate.rs"),
     )
-    .expect("failed to write to build.rs");
+    .expect("jessie-build : failed to write to build.rs");
 }
 
 fn create_file_at_dir(projectname: &str, filename: &str) -> std::io::Result<File> {
